8283a1ff0f21dc0d9da2823f2fcd5d66349d94d0
Serhii Slipchenko <sliptchenko71@gmail.com>
2022-07-30 22:48:28 +0300

    flask

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-import aiohttp
-from aiohttp import web, WSMessage
+from flask import Flask
+from flask_sock import Sock
+
+from simple_websocket.ws import Server
+
+app = Flask(__name__)
+sock = Sock(app)
 
 
-async def websocket_handler(request):
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
+@sock.route('/reverse')
+def reverse(ws: Server):
+    print(type(ws))
 
-    async for msg in ws:
-        msg: WSMessage = msg
-        print(msg)
-
-        if msg.type == aiohttp.WSMsgType.TEXT:
-            if msg.data == 'close':
-                await ws.close()
-            else:
-                await ws.send_str(msg.data + '/answer')
-        elif msg.type == aiohttp.WSMsgType.ERROR:
-            print('ws connection closed with exception %s' %
-                  ws.exception())
-
-    print('websocket connection closed')
-
-    return ws
-
-
-application = web.Application()
-application.add_routes([
-    web.get('/ws', websocket_handler)
-])
-
-# web.run_app(application)
+    text = ws.receive()
+    ws.send(text[::-1])
